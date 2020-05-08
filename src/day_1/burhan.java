ed3ab7c1dd8545a8d98c371bb1239c7d76274904
cny1024 <cihadnaciyldrm@gmail.com>
2020-05-08 14:39:57 -0500

    update burhan.java

--- src/day_1/burhan.java
+++ src/day_1/burhan.java
@@ -4,5 +4,6 @@ public class burhan {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("githubdan selamlar");
+        System.out.println("sa hacim");
     }
 }
